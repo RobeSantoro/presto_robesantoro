0c5b138a57845659d39b7e2dc708993dad42aed7
Robe Santoro <robe.santoro@live.com>
2020-06-17 18:31:02 +0200

    Prima del Migration

--- resources/js/myScript.js
+++ resources/js/myScript.js
@@ -4,14 +4,11 @@ let AdvancedFormGroup = document.querySelector("#AdvancedFormGroup");
 let SearchInput = document.querySelector("#SearchInput");
 let SearchLabel = document.querySelector("#SearchLabel");
 
-
-
-
 AdvancedBtn.addEventListener('click', () => {
-    
+
     /* AdvancedFormGroup.classList.toggle("collapse"); */
-    /* Fatto poi con Bootstrap Class Collapse*/ 
-    
+    /* Fatto poi con Bootstrap Class Collapse*/
+
     SearchInput.classList.toggle("form-control-lg");
     SearchLabel.classList.toggle("d-none");
 
