b7c4bb136d53db671cc1d22f59cb5ca3763f6bd5
Robe Santoro <robe.santoro@live.com>
2020-06-22 21:13:42 +0200

    make:migration add_used_id_column_to_products

---
+++ database/migrations/2020_06_22_182726_add_used_id_column_to_products.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddUsedIdColumnToProducts extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('products', function (Blueprint $table) {
+
+            // Aggiunge la colonna user_id alla tabella products
+            $table->unsignedBigInteger('user_id')->default(1);
+
+            // Specifica che colonna 'user_id' appena creata
+            // è una chiave esterna che fa riferimento alla colonna 'id' alla tabella 'users'
+            $table->foreign('user_id')->references('id')->on('users');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()  // La funzione down è l'esatto contrario della funzione down.
+    {
+        Schema::table('products', function (Blueprint $table) {
+
+            $table->dropForeign(['user_id']);   // Questa rifga canella la relazione
+            $table->dropColumn('user_id');      // Questa riga cancella la colonna
+
+        });
+    }
+}
