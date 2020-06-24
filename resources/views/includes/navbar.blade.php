bbc2e91956284986d7ccc94f9d142e56ba27b877
Robe Santoro <robe.santoro@live.com>
2020-06-24 11:10:45 +0200

    Fixed Login link in navbar

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -40,20 +40,19 @@
             <ul class="navbar-nav ml-auto">
 
                 <!-- Authentication Links -->
-                @guest
-                    <li class="nav-item dropdown">
-                        <a id="navbarDropdown" class="nav-link" href="{{ route('login') }}" role="button"
-                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                        {{ __('Login') }}
-                    </a>
-                </li>
-                @if(Route::has('register'))
-                <li class="nav-item">
-                    <a class="nav-link"
-                    href="{{ route('register') }}">{{ __('Register') }}</a>
-                </li>
-                @endif
+                 @guest
+                    <li class="nav-item">
+                        <a class="nav-link"
+                            href="{{ route('login') }}">{{ __('Login') }}</a>
+                    </li>
+                    @if(Route::has('register'))
+                        <li class="nav-item">
+                            <a class="nav-link"
+                                href="{{ route('register') }}">{{ __('Register') }}</a>
+                        </li>
+                    @endif
                 @else
+
                 <li class="nav-item dropdown">
                     <a id="navbarDropdown" class="nav-link" href="#" role="button"
                     data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
