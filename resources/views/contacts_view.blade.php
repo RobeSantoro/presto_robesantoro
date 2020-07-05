7b1abe7c2dffeff88546e1477c9d20ec1a00c212
Robe Santoro <robe.santoro@live.com>
2020-07-05 22:11:11 +0200

    Errors per field @if($errors->has('name')) <p>{{ $errors->first('name') }}</p>

--- resources/views/contacts_view.blade.php
+++ resources/views/contacts_view.blade.php
@@ -23,27 +23,47 @@
                     <form method="POST" action="{{ route('submit_route') }}" enctype="multipart/form-data">
                         @csrf
 
-                        <label class="float-left mt-3 mb-0 mx-1" for="exampleFormControlSelect1">Nome</label>
+                        <label class="float-left mt-2 mb-0 mx-1" for="exampleFormControlSelect1">Nome</label>
                         <input class="form-control" type="text" placeholder="Inserisci Nome" name="name"
-                            value="{{ old('name') }}">
+                        value="{{ old('name') }}">
 
-                        <label class="float-left mt-3 mb-0 mx-1" for="exampleFormControlSelect1">Cognome</label>
+                        @if($errors->has('name'))
+                            <p class="m-0" style="color:red;" >{{ $errors->first('name') }}</p>
+                        @endif
+
+                        <label class="float-left mt-2 mb-0 mx-1" for="exampleFormControlSelect1">Cognome</label>
                         <input class="form-control" type="text" placeholder="Inserisci Cognome" name="surname"
                             value="{{ old('surname') }}">
 
-                        <label class="float-left mt-3 mb-0 mx-1" for="exampleFormControlSelect1">Email</label>
+                        @if($errors->has('surname'))
+                            <p class="m-0" style="color:red;" >{{ $errors->first('surname') }}</p>
+                        @endif
+
+                        <label class="float-left mt-2 mb-0 mx-1" for="exampleFormControlSelect1">Email</label>
                         <input class="form-control" type="email" placeholder="Inserisci e-mail" name="email"
                             value="{{ old('email') }}">
 
-                        <label class="float-left mt-3 mb-0 mx-1" for="exampleFormControlSelect1">Mobile</label>
+                        @if($errors->has('email'))
+                            <p class="m-0" style="color:red;" >{{ $errors->first('email') }}</p>
+                        @endif
+
+                        <label class="float-left mt-2 mb-0 mx-1" for="exampleFormControlSelect1">Mobile</label>
                         <input class="form-control" type="tel" placeholder="Inserisci Tel" name="mobile"
                             value="{{ old('mobile') }}">
 
-                        <label class="float-left mt-3 mb-0 mx-1" for="exampleFormControlSelect1">Foto</label>
+                        @if($errors->has('mobile'))
+                            <p class="m-0" style="color:red;" >{{ $errors->first('mobile') }}</p>
+                        @endif
+
+                        <label class="float-left mt-2 mb-0 mx-1" for="exampleFormControlSelect1">Foto</label>
                         <input class="form-control" type="file" placeholder="Inserisci una tua foto" name="photo"
                             value="{{ old('photo') }}">
 
-                        @if($errors->any())
+                        @if($errors->has('photo'))
+                            <p class="m-0" style="color:red;" >{{ $errors->first('photo') }}</p>
+                        @endif
+
+                        {{-- @if($errors->any())
 
                             <div class="my-4">
 
@@ -54,7 +74,7 @@
                                 </ul>
 
                             </div>
-                        @endif
+                        @endif --}}
 
                         <button type="reset"  class="btn btn-secondary mt-5 px-5">Reset</button>
                         <button type="submit" class="btn btn-primary mt-5 px-5">Invia</button>
