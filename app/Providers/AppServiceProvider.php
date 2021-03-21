888a637a51cf871b54519bc0950c568e03358ef8
Robe Santoro <robe.santoro@live.com>
2021-03-21 13:29:19 +0100

    TEMP FIX - Composer Install

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,9 +16,9 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot()
     {
-        if (Schema::hasTable('categories')) {
+   /*      if (Schema::hasTable('categories')) {
             $categories = Category::all();
             View::share('categories', $categories);
-        } 
+        }  */
     }
 }
